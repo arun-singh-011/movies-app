f6269c082236e199ba6796f92c7e6cb391842454
ARUN SINGH <arunsinghh0011@gmail.com>
2025-11-16 20:50:39 -0800

    Feature: Api link Added and movies are now loaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,20 @@ import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 // import { MovieProvider } from "./contexts/MovieContext";
-// import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/favorites" element={<Favorites />}></Route>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,29 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const movies = [
-    { id: 1, title: "John wick", release_date: "2020" },
-    { id: 2, title: "La La Land", release_date: "2009" },
-    { id: 3, title: "Avengers", release_date: "2023" },
-    { id: 4, title: "Happy Gilmore", release_date: "2018" },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     alert(searchQuery);
